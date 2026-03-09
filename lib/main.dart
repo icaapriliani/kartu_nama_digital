ac136b40def61a6896e93a98ed0c89517dbac02a
icaapriliani <icaaprilianikd.com>
2026-03-09 22:36:36 +0700

    tahap 2, Container putih sebgai Kartu  dengan efek bayangan dan penanganan overflow

--- lib/main.dart
+++ lib/main.dart
@@ -25,8 +25,33 @@ class profileCard extends StatelessWidget{
     // TODO: implement build
     return Scaffold(
       backgroundColor: Colors.grey[100],
-      body: const Center(
-        child: Text ("Scaffold dengan warna background abu-abu terang."),
+      body:  Center(
+        //dibungkus dgn center agar kartu tetap ditengah,dan tetap bisa di scroll jika konten melebihi tinggi layar
+        child: SingleChildScrollView(
+          child:Container(
+            margin:const EdgeInsets.all(20), //mencegah kartu menempel ketepi layar hp kecil
+            padding: const EdgeInsets.all(30),
+            width: 350,
+            decoration: BoxDecoration(
+              color:  Colors.white,
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+                BoxShadow(
+                  color:Colors.black.withOpacity(0.1), //efek bayangan halus
+                  spreadRadius: 2,
+                  blurRadius: 15,
+                  offset: const Offset(0, 5), //bayangan jatuh sedikit kebawah
+                ),
+              ],
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min, //sesuaikan tinggi kartu dengan konten
+              children: const[
+              Text("kartu putih"), 
+              ],
+            ),
+          ),
+        ),
 
     ),
     );
